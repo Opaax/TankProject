eacf223e49b52155f64d7c66de3e22c4e3944f5d
Enguerran <61538252+Opaax@users.noreply.github.com>
2023-02-14 17:56:08 +0100

    some effect on shoot

--- Source/TankGame/Private/Camera/TankCameraController.cpp
+++ Source/TankGame/Private/Camera/TankCameraController.cpp
@@ -2,13 +2,16 @@
 
 
 #include "Camera/TankCameraController.h"
+#include "TankGame/Public/PlayerController/TankBasePlayerController.h"
+
+#include "Camera/CameraShakeBase.h"
 
 // Sets default values for this component's properties
 UTankCameraController::UTankCameraController()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -32,3 +35,20 @@ void UTankCameraController::TickComponent(float DeltaTime, ELevelTick TickType, 
 	// ...
 }
 
+void UTankCameraController::InitCameraController(ATankBasePlayerController* PlayerControllerTank)
+{
+	m_playerTankController = PlayerControllerTank;
+}
+
+void UTankCameraController::PlayShakePrimaryShoot()
+{
+	if (m_primaryShakeClass)
+	{
+		m_playerTankController->ClientPlayCameraShake(m_primaryShakeClass);
+	}
+}
+
+void UTankCameraController::PlayShakeSecondaryShoot()
+{
+	
+}
